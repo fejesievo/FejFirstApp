140f0e6187098aa9ac185a46e7fc99fcd5268556
Fej Esievo <116123894+fejesievo@users.noreply.github.com>
2024-07-17 16:36:03 -0400

    Initial Commit

---
+++ FejFirstApp/FejFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FejFirstAppApp.swift
+//  FejFirstApp
+//
+//  Created by Fejiro Esievo on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FejFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
